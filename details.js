bc5687a580f3267f96682b590af8b2ef38278a8a
dragan717080 <dburic717@outlook.com>
2025-06-28 17:47:59 +0200

    adding input handling on `details` page

--- details.js
+++ details.js
@@ -1,5 +1,75 @@
+/**
+ * Checks whether any pay option is selected, and returns it if it is.
+ *
+ * @returns {string | undefined}
+ */
+const checkPayOptionIsActive = () => {
+  // If neither pay option is selected, raise an error
+  const paypalContainer = document.getElementById('paypal-container');
+  const cashContainer = document.getElementById('cash-container');
+
+  const payElements = [paypalContainer, cashContainer];
+
+  const isAnyPayOptionActive = payElements.find(payElement => payElement.style.backgroundColor === 'rgb(225, 89, 44)' || cashContainer.style.backgroundColor === 'rgb(225, 89, 44)');
+
+  const selectPayErrorElement = document.getElementById('select-pay-error');
+  console.log('isAnyPayOptionActive:', isAnyPayOptionActive);
+  if (!isAnyPayOptionActive) {
+    selectPayErrorElement.classList.remove('hidden');
+
+    return false;
+  } else {
+    selectPayErrorElement.classList.add('hidden');
+    return isAnyPayOptionActive.innerText;
+  }
+}
+
+
+/**
+ * Checks whether the selected start time is not after end time.
+ *
+ * @returns {Array<number>|false} - Array of start and end time if it is correctly selected, otherwise false.
+ */
+const checkHoursAndMinutes = () => {
+  const selectedStartHour = parseInt(document.querySelector("select[name='hour-start']").value);
+  const selectedEndHour = parseInt(document.querySelector("select[name='hour-end']").value);
+  const selectedStartMinute = parseInt(document.querySelector("select[name='minute-start']").value);
+  const selectedEndMinute = parseInt(document.querySelector("select[name='minute-end']").value);
+
+  const selectedTimeError = document.getElementById('select-time-error');
+
+  const hourStartIsBigger = selectedStartHour > selectedEndHour;
+  const hoursAreSame = selectedStartHour === selectedEndHour;
+  const minuteStartSameOrEqual = selectedStartMinute >= selectedEndMinute;
+
+  // TO DO: Check that the worker works at least 1 hour
+  // TO DO: Check that the days in months are ok (e.g. not more than days in that month)
+
+  if (hourStartIsBigger || hoursAreSame && minuteStartSameOrEqual) {
+    selectedTimeError.classList.remove('hidden');
+
+    return false;
+  } else {
+    selectedTimeError.classList.add('hidden');
+
+    return [
+      selectedStartHour,
+      selectedStartMinute,
+      selectedEndHour,
+      selectedEndMinute
+    ];
+  }
+}
+
+
 const hire = () => {
-  console.log('1');
+  const selectedPayOption = checkPayOptionIsActive();
+  const selectedTime = checkHoursAndMinutes();
+  console.log('selectedTime:', selectedTime);
+
+  if (!selectedPayOption || !selectedTime) {
+    return false;
+  }
 }
 
 /**
@@ -20,6 +90,10 @@ const onlyGiveBackgroundToCurrentlyActiveInput = (
   activeElement.style.backgroundColor = '#E1592C';
   activeElement.style.border = '1px solid #FFF';
   activeElement.style.color = '#FFF';
+  // However, don't change the color on `select`
+  Array.from(activeElement.getElementsByTagName('select')).forEach(selectElement => {
+    selectElement.style.color = '#9C9C9C';
+  });
 
   // Give default to other elements
   otherElements.forEach(otherElement => {
@@ -31,15 +105,27 @@ const onlyGiveBackgroundToCurrentlyActiveInput = (
 
 
 const addListenersForPaypalAndCash = () => {
-  const allOptions = [
+  const dateAndTimeOptions = [
     document.getElementById('date-container'),
     document.getElementById('time-container'),
+  ];
+
+  dateAndTimeOptions.forEach((activeElement) =>
+    activeElement.addEventListener('click', () => onlyGiveBackgroundToCurrentlyActiveInput(dateAndTimeOptions, activeElement))
+  );
+
+  const payOptions = [
     document.getElementById('paypal-container'),
     document.getElementById('cash-container')
   ];
 
-  allOptions.forEach((activeElement) =>
-    activeElement.addEventListener('click', () => onlyGiveBackgroundToCurrentlyActiveInput(allOptions, activeElement))
+  payOptions.forEach((activeElement) => {
+    // Remove the `select pay option` error as soon as any option is selected
+    const selectPayErrorElement = document.getElementById('select-pay-error');
+
+    selectPayErrorElement.classList.add('hidden');
+    activeElement.addEventListener('click', () => onlyGiveBackgroundToCurrentlyActiveInput(payOptions, activeElement))
+  }
   );
 }
 
